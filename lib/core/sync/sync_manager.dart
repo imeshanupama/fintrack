994e86319e0ef0439a88b41346e10359433d78bb
Imesh Rathnayake <rmialmbs@gmail.com>
2026-01-16 12:24:12 +0100

    Implement Recurring Transactions and Fix Analytics UI

--- lib/core/sync/sync_manager.dart
+++ lib/core/sync/sync_manager.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../features/auth/presentation/auth_provider.dart';
 import 'sync_provider.dart';
+import '../../features/recurring/application/recurring_transaction_service.dart';
 
 class SyncManager extends ConsumerStatefulWidget {
   final Widget child;
@@ -20,6 +21,7 @@ class _SyncManagerState extends ConsumerState<SyncManager> {
       final user = ref.read(authStateProvider).value;
       if (user != null) {
          ref.read(syncServiceProvider).init();
+         ref.read(recurringTransactionServiceProvider).checkAndGenerateTransactions();
       }
     });
   }
@@ -29,10 +31,12 @@ class _SyncManagerState extends ConsumerState<SyncManager> {
     ref.listen(authStateProvider, (previous, next) {
       final user = next.value;
       final syncService = ref.read(syncServiceProvider);
+      final recurringService = ref.read(recurringTransactionServiceProvider);
       
       if (user != null) {
-        debugPrint("User logged in. Initializing Sync.");
+        debugPrint("User logged in. Initializing Sync & Recurring.");
         syncService.init();
+        recurringService.checkAndGenerateTransactions();
       } else {
         debugPrint("User logged out. Disposing Sync.");
         syncService.dispose();
